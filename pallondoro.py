02b8c177878ad99b1793bbd16cae84b2ba3a5ae8
Roberto Saleri <33120404+rsaleri@users.noreply.github.com>
2024-11-26 22:44:58 +0100

    better naming

--- pallondoro.py
+++ pallondoro.py
@@ -83,13 +83,13 @@ class Dice:
         for d in temp_dices:
             match_re = re.match(r"([1-9][0-9]*)[dD]([1-9][0-9]*)", d)
             if match_re:
-                g1 = int(match_re.group(1))
-                g2 = int(match_re.group(2))
+                dice_faces = int(match_re.group(1))
+                dice_qty = int(match_re.group(2))
                 # fallback if instead of ndn the called for dn
-                if g1 == 0:
-                    g1 = 1
+                if dice_qty == 0:
+                    dice_qty = 1
                 
-                this_dices = [g2] * g1
+                this_dices = [dice_faces] * dice_qty
                 self.dices.extend(this_dices)
 
     def roll(self) -> int:
